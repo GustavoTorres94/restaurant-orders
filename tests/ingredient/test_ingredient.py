e0ed9ad9818787381993529628cad1c58e414ef5
Gustavo <gustavo.t.paula@gmail.com>
2024-02-14 14:32:45 -0300

    test2

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,11 +4,8 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 def test_ingredient():
     ingredient = Ingredient("Tomato")
     assert ingredient.name == "Tomato"
-    assert ingredient.__str__() == "Tomato"
-    assert ingredient.__repr__() == "Tomato"
+    assert ingredient.__repr__() == "Ingredient('Tomato')"
     assert ingredient.restrictions == set()
     assert ingredient.__hash__() == hash("Tomato")
-    assert ingredient.__eq__("Tomato") is True
-    assert ingredient.__eq__("Onion") is False
     assert ingredient.__eq__(Ingredient("Tomato")) is True
     assert ingredient.__eq__(Ingredient("Onion")) is False
